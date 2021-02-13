6ec752a55fef694141ec3fa0109aea1d874703c2
rishab singh <rishabhsinngh123@gmail.com>
2021-02-13 18:08:38 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import history from './routes/history';
+import './assets/scss/custom.scss';
+import './index.css';
+import Routes from './routes/routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +25,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
